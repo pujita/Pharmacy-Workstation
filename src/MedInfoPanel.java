d7aa6ce2b097ec8577cacae6d157172ade7c1a98
Pujita Vijayvargiya <pujita90@gmail.com>
2011-10-31 01:00:49 -0400

    inputing patient id shows the patient info and the medicine info of the patient

--- src/MedInfoPanel.java
+++ src/MedInfoPanel.java
@@ -8,15 +8,35 @@ public class MedInfoPanel extends JPanel {
 	JLabel dose;
 	JLabel time;
 	
-	public MedInfoPanel(Medicine med){
+	public MedInfoPanel(){
+		//this.med = med;
+		this.medName = new JLabel("");
+		this.dose = new JLabel("");
+		this.time = new JLabel("");
+		/*
 		this.med = med;
 		this.medName = new JLabel(med.getName());
 		this.dose = new JLabel(med.getDose() + "mg");
 		this.time = new JLabel(med.getTimeString());
-		
+		*/
 		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 		add(medName);
 		add(time);
 		add(dose);
+	}
+	
+	public void panelReset(){
+		this.medName.setText("");
+		this.dose.setText("");
+		this.time.setText("");
+
+	}
+	
+	public void setPanel(Medicine med){
+		this.med = med;
+		this.medName.setText(med.getName());
+		this.dose.setText(med.getDose() + "mg");
+		this.time.setText(med.getTimeString());
+
 	}
 }
--- src/MultiMedsPanel.java
+++ src/MultiMedsPanel.java
@@ -3,16 +3,36 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class MultiMedsPanel extends JPanel{
 	
-	public MultiMedsPanel(){
+	MedInfoPanel[] panel = new MedInfoPanel[5];
 		
+	public MultiMedsPanel(){
+		panel[0] = new MedInfoPanel();
+		panel[1] = new MedInfoPanel();
+		panel[2] = new MedInfoPanel();
+		panel[3] = new MedInfoPanel();
+		panel[4] = new MedInfoPanel();
+
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		add(panel[0]);
+		add(panel[1]);
+		add(panel[2]);
+		add(panel[3]);
+		add(panel[4]);
 	}
 	
 	public void setMedsPanel(Patient p){
+		panel[0].panelReset();
+		panel[1].panelReset();
+		panel[2].panelReset();
+		panel[3].panelReset();
+		panel[4].panelReset();
 		
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		
+		//setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		int count = 0;
 		for (String key : p.getMeds().keySet()) {
-			add(new MedInfoPanel(p.getMeds().get(key)));
+			panel[count].setPanel(p.getMeds().get(key));
+			//add(new MedInfoPanel(p.getMeds().get(key)));
+			count++;
 		}	
 	}
 }
