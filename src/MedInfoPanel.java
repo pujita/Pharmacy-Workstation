05f3baf174be334cf8ef955428f5be15ccf4bc62
Pujita Vijayvargiya <pujita90@gmail.com>
2011-11-13 20:01:11 -0500

    Documented Main three frames

--- src/MedInfoPanel.java
+++ src/MedInfoPanel.java
@@ -5,22 +5,26 @@ public class MedInfoPanel extends JPanel {
 	
 	Medicine med;
 	JLabel medName;
+	JLabel id;
 	JLabel dose;
 	JLabel time;
 	
 	public MedInfoPanel(){
 		
+		this.id = new JLabel("");
 		this.medName = new JLabel("");
 		this.dose = new JLabel("");
 		this.time = new JLabel("");
 		
 		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+		add(id);
 		add(medName);
 		add(time);
 		add(dose);
 	}
 	
 	public void panelReset(){
+		this.id.setText("");
 		this.medName.setText("");
 		this.dose.setText("");
 		this.time.setText("");
@@ -30,6 +34,7 @@ public class MedInfoPanel extends JPanel {
 	public void setPanel(Medicine med){
 		this.med = med;
 		this.medName.setText(med.getName());
+		this.id.setText(med.getMedID());
 		this.dose.setText(med.getDose() + "mg");
 		this.time.setText(med.getTimeString());
 
